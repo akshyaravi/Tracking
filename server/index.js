1f9b98e6caacf96b7ec0d32986e7c0296a594157
akshyaravi <r.akshyaravi10@gmail.com>
2025-10-26 13:03:39 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -20,8 +20,8 @@ connectDB();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'development'
-    ? ['https://tracking-mu-six.vercel.app', 'https://tracking-yr4b.vercel.app']
-    : ['http://localhost:3000'],
+    ? ['http://localhost:3000']
+    : ['https://tracking-mu-six.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
